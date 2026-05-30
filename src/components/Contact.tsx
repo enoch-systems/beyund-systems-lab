2bb077618d18ea7ceaf3b716af22a46ce86a066f
Enoch Chukwudi <enochc.official@gmail.com>
2026-05-30 19:46:54 +0100

    Update Sex dropdown to only Male and Female options

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -296,7 +296,6 @@ export default function Contact() {
                       <option value="" disabled>Select your sex</option>
                       <option value="male">Male</option>
                       <option value="female">Female</option>
-                      <option value="other">Other</option>
                     </select>
                     <svg className="pointer-events-none absolute right-4 top-1/2 -translate-y-1/2 w-4 h-4 text-white/50" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2.5}>
                       <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
