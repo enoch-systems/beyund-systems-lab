71c59a498690f1fc6a624b73c2b663b8706377d3
Enoch Chukwudi <enochc.official@gmail.com>
2026-05-30 08:24:23 +0100

    Make project cards 1 column on mobile

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -71,7 +71,7 @@ export default function Projects() {
         </div>
 
         {/* Project grid */}
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 md:gap-4">
+<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 md:gap-4">
           {projects.map((project, i) => (
             <div
               key={project.name}
