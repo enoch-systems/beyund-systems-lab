b93d878371c8d9526fbf6832b3ce7069044588d8
Enoch Chukwudi <enochc.official@gmail.com>
2026-05-30 15:06:23 +0100

    fix typo: SocLialSidebar -> SocialSidebar in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
       <div className="relative z-10">
         <Navbar />
         <Hero>
-          <SocLialSidebar />
+          <SocialSidebar />
         </Hero>
         <Layers />
         <About />
