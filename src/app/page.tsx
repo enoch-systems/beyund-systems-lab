67922ff29c5ba57cc5b03f97f52a754c9fe66ea5
Enoch Chukwudi <enochc.official@gmail.com>
2026-05-30 05:10:28 +0100

    Change background layers from absolute to fixed to eliminate white space after footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,11 @@ import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className="relative min-h-screen bg-white">
-      {/* Full-page background spanning Hero to Footer */}
-      <div className="absolute inset-0 h-full bg-black/80" />
+    <main className="relative min-h-screen bg-black">
+      {/* Fixed background covering entire page */}
+      <div className="fixed inset-0 bg-black/80" />
       <div
-        className="absolute inset-0 h-full"
+        className="fixed inset-0"
         style={{
           backgroundImage: "url('https://res.cloudinary.com/djdbcoyot/image/upload/v1779492891/tvchvy2zbigbduggdlmi.jpg')",
           backgroundSize: "cover",
@@ -25,8 +25,8 @@ export default function Home() {
           transform: "scale(1.05)",
         }}
       />
-      {/* Gradient overlay spanning entire page */}
-      <div className="absolute inset-0 h-full bg-gradient-to-b from-black/60 via-black/40 to-black/80" />
+      {/* Fixed gradient overlay */}
+      <div className="fixed inset-0 bg-gradient-to-b from-black/60 via-black/40 to-black/80" />
 
       <div className="relative z-10">
         <Navbar />
